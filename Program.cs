5c6c6ed3a0be03f387d90990124e487190f8653a
Roberto Josafat Vallejo Reyes <vallejoroberto19@gmail.com>
2025-06-28 18:22:18 -0500

    update x2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 using ProductosAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar DbContext para PostgreSQL
 builder.Services.AddDbContext<ProductosContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.MapControllers(); // Muy importante para que los endpoints funcionen
+// Asegúrate de que se apliquen las migraciones al arrancar (opcional)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ProductosContext>();
+    db.Database.Migrate(); // Aplica migraciones si existen
+}
+
+app.MapControllers();
 
 app.Run();
